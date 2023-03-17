34b7c8090966244e8f55807b9eb267bfd1f47478
Joao Euko <joaopeuko@gmail.com>
2023-03-17 08:58:33 +0000

    Update name of an argument and add new items to the dictionary

--- pdtj/json_generator.py
+++ pdtj/json_generator.py
@@ -1,6 +1,7 @@
 import os
 import __version__
 from pdtj.files_finder import get_directory_type, files_parser
+from pdtj.parameters import parameters
 from pdtj.util import create_docs_and_pdtj_dir
 
 import argparse
@@ -9,23 +10,26 @@ import argparse
 def get_arguments():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--root",
+    parser.add_argument("--project_name",
                         type=str,
-                        action="store")
+                        action="store",
+                        default=parameters['name'])
 
     return vars(parser.parse_args())
 
 
 def main():
 
-    root = get_arguments()['root']
+    project_name = get_arguments()['project_name']
 
     create_docs_and_pdtj_dir()
 
-    directory_dict = {root: {"type": get_directory_type(f"../{root}")}}
-    path = f"./{root}"
-    upper_file = root
+    directory_dict = {project_name: {"type": get_directory_type(f"../{project_name}")}}
+    path = f"./{project_name}"
+    upper_file = project_name
     file_parser_result = files_parser(path, upper_file, directory_dict)
+
+    file_parser_result[project_name].update({"shelve": parameters['shelve'], "sub-project": parameters['sub-project']})
 
     path = os.path.join(os.getcwd(), 'docs')
     path = os.path.join(path, 'pdtj')
