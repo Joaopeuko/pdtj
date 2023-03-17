5e3321576cfd687f23d45e9a1cfb424a8ee3040b
Joao Euko <joaopeuko@gmail.com>
2023-03-17 08:56:57 +0000

    Update how requirements are generated

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import json
+import os
 import pathlib
 import setuptools
 
@@ -5,8 +7,32 @@ import __version__
 
 long_description = (pathlib.Path(__file__).parent / "README.md").read_text()
 
-with open("requirements.txt", "r") as requirements_file:
-    requirements = requirements_file.readlines()
+
+# with open("requirements.txt", "r") as requirements_file:
+#     requirements = requirements_file.readlines()
+#
+def get_requirements_from_pipfile_lock(pipfile_lock=None):
+    if pipfile_lock is None:
+        pipfile_lock = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'Pipfile.lock')
+    lock_data = json.load(open(pipfile_lock))
+    retval = []
+    for package_name, package_data in lock_data.get('default', {}).items():
+        if package_data.get('file'):
+            package_ref = f'{package_name} @ {package_data["file"]}'
+        else:
+            package_ref = f'{package_name}{package_data["version"]}'
+        retval.append(package_ref)
+    return retval
+
+
+def generate_requirements_txt(pipfile_lock_requirements):
+    for requirement in pipfile_lock_requirements:
+        with open(os.path.join('requirements.txt'), 'w') as file:
+            file.writelines(requirement)
+
+
+requirements = get_requirements_from_pipfile_lock()
+generate_requirements_txt(requirements)
 
 setuptools.setup(
     name='pdtj',
@@ -18,7 +44,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     packages=setuptools.find_packages(),
-    install_requires=[requirement for requirement in requirements],
+    install_requires=requirements,  # [requirement for requirement in requirements]
     entry_points={
         "console_scripts": [
             "pdtj = pdtj.json_generator:main",
