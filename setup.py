66134adef4dce0dd0341ae6dd1fff60761e8fddb
Joao Euko <joaopeuko@gmail.com>
2023-03-21 10:50:56 +0000

    bump: version 0.3.2 → 0.3.3

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ generate_requirements_txt(requirements)
 
 setuptools.setup(
     name="pdtj",
-    version="0.3.2",
+    version="0.3.3",
     license="MIT",
     author="Joao Paulo Euko",
     url="https://github.com/Joaopeuko/pdtj",
